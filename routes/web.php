64f1f451e92092bf54a73a7b194956a1f53222da
Mario Kujundzic <mario.kujundzic.su@gmail.com>
2020-10-07 14:23:07 +0200

    #LAGP-7 Add missing route names

--- routes/web.php
+++ routes/web.php
@@ -15,26 +15,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/vivify', function () {
     echo "<h1>You are GET-ing to Vivify!</h1>";
-});
+})->name('vivifyGet');
 
 Route::post('/vivify', function () {
     echo "<h1>You are POST-ing to Vivify!</h1>";
-});
+})->name('vivifyPost');
 
 Route::put('/vivify', function () {
     echo "<h1>You are PUT-ing to Vivify!</h1>";
-});
+})->name('vivifyPut');
 
 Route::patch('/vivify', function () {
     echo "<h1>You are PATCH-ing to Vivify!</h1>";
-});
+})->name('vivifyPatch');
 
 Route::delete('/vivify', function () {
     echo "<h1>You are DELETE-ing to Vivify!</h1>";
-});
+})->name('vivifyDelete');
 
 
