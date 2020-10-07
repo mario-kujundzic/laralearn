fa19ae35bbb6a112296543e5846f10af22b06866
Mario Kujundzic <mario.kujundzic.su@gmail.com>
2020-10-07 14:51:56 +0200

    #LAGP-8 Add logging of incoming requests via middleware

---
+++ app/Http/Middleware/LogRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class LogRequest
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        info(print_r($request, true));
+        return $next($request);
+    }
+}
